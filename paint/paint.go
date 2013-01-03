bcd8b13d63da26747db71f8678841f0a14028f79
Ben L <nightgunner5@llamaslayers.net>
2013-01-03 15:51:26 -0600

    optimize the optimized fade

--- paint/paint.go
+++ paint/paint.go
@@ -24,35 +24,14 @@ func paint(dst *image.RGBA) {
 	frame++
 }
 
-var fadeMap [256]uint8
-
-func init() {
-	const (
-		m = 255
-		a = m - 8
-	)
-
-	for i := range fadeMap {
-		fadeMap[i] = uint8(uint16(i) * a / m)
-	}
-}
-
 // A more efficient way of obtaining the same result as drawing
 // color.RGBA{0, 0, 0, 8} over every pixel.
 func fade(dst *image.RGBA) {
-	i0 := dst.PixOffset(0, 0)
-	i1 := i0 + dst.Bounds().Dx()*4
+	const (
+		a = 256 - 8
+	)
 
-	dy := dst.Bounds().Dy()
-
-	for ; dy > 0; dy-- {
-		for i := i0; i < i1; i += 4 {
-			dst.Pix[i+0] = fadeMap[dst.Pix[i+0]]
-			dst.Pix[i+1] = fadeMap[dst.Pix[i+1]]
-			dst.Pix[i+2] = fadeMap[dst.Pix[i+2]]
-		}
-
-		i0 += dst.Stride
-		i1 += dst.Stride
+	for i, b := range dst.Pix {
+		dst.Pix[i] = uint8((uint(b) * a) >> 8)
 	}
 }
