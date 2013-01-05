3c9feea2c82d426eb648ff06eda0d61746e3a619
Ben L <nightgunner5@llamaslayers.net>
2013-01-04 21:42:37 -0600

    tweak the email

--- strongbad_email.go
+++ strongbad_email.go
@@ -9,9 +9,9 @@ func init() {
 	commands["strongbad_email.exe"] = func([]string) {
 		paint.WithConsole(func(c *console.Console) {
 			c.Println("Dear Stong Bag,")
-			c.Println("Do you like crap? Do you like the word crap?")
-			c.Println("Sometimes I like to write about crap. Isn't")
-			c.Println("crap craptastic? Crap is the crap.")
+			c.Println("Do you like crap? Did you invent the word crap?")
+			c.Println("Sometimes I like to write about crap. Isn't crap")
+			c.Println("craptastic? Crap is the crap. Crap crap crap crap.")
 			c.Println()
 			c.Println("Crapfully crapfully,")
 			c.Println("Dennis")
